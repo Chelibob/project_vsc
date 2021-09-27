2c5ded540a2493fdcd2d328846284b293751fe77
Chelibob <nicekolya.a7x@gmail.com>
2021-09-28 00:26:58 +0500

    Clear main function

--- main.cpp
+++ main.cpp
@@ -3,13 +3,6 @@
 
 int main()
 {
-     std::cout << "Input a keyboard character: "; // предположим, что пользователь ввел abcd
- 
-    char ch;
-    std::cin >> ch; // ch = 'a', "bcd" останется во входном буфере
-    std::cout << ch << " has ASCII code " << static_cast<int>(ch) << std::endl;
- 
-    // Обратите внимание, следующий cin не просит пользователя что-либо ввести, данные берутся из входного буфера!
-
+    
     return 0;
 }
