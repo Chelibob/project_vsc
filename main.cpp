721c4e28951a4687d7da5e4dd470067300f38b4b
Chelibob <nicekolya.a7x@gmail.com>
2021-10-07 23:58:35 +0500

    Practic exercise. Trip calculator

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <iomanip>
+#include <windows.h>
 #include "add.h"
+#include "partone.hpp"
 
 int main()
 {
-    
+    SetConsoleCP(65001);   
+	SetConsoleOutputCP(65001);
+    float distance, consumption, pricePerLitre;
+    std::cout << "Расстояние до дачи (км): ";
+    std::cin >> distance;
+    std::cout << "Расход бензина (литров на 100 км пробега): ";
+    std::cin >> consumption;
+    std::cout << "Цена литра бензина (руб.): ";
+    std::cin >> pricePerLitre;
+    std::cout << "Поездка на дачу и обратно обойдется в " << std::setprecision(4) << tripCalculator(distance,consumption,pricePerLitre) << " руб.";
     return 0;
 }
---
+++ partone.hpp
@@ -0,0 +1,4 @@
+#ifndef PARTONE_HPP
+#define PARTONE_HPP
+float tripCalculator(float,float,float);
+#endif
