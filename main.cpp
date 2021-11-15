3587a845d6e74f61786025afb18d69b7a3c091d4
Chelibob <nicekolya.a7x@gmail.com>
2021-11-15 22:55:28 +0500

    Delete BMI program, add programm degrees converter

--- main.cpp
+++ main.cpp
@@ -7,43 +7,27 @@
 #include <vector>
 #include <cstdint>
 
-float getFloat()
-{
-  std::cout << "Enter the weight in pounds " ;
-  float iNumber;
-  std::cin >> iNumber;
-  return iNumber;
-}
 
-int getInchs()
+int getNumber()
 {
-  std::cout << "Enter the height in inchs " ;
   int iNumber;
   std::cin >> iNumber;
   return iNumber;
 }
 
-int getFoots()
-{
-  std::cout << "Enter the height in foots " ;
-  int iNumber;
-  std::cin >> iNumber;
-  return iNumber;
-}
-
+//Программа далека от идельной, многие вещи здесь можно переделать
+//НО функциональность, которая от нее требуется она выполняет
 int main()
 {
-  int heightFoots = getFoots();
-  int heightInchs = getInchs();
-  float weight = getFloat();
-  const int footInInch = 12;
-  const float metersCoef = 0.0254;
-  const float weightCoef = 2.2;
-  float heightInMeters = (heightFoots * footInInch + heightInchs) * metersCoef;
-  float weightInKilo = weight / weightCoef;
-  float bodyMassIndex = weightInKilo / (heightInMeters * heightInMeters);
-  std::cout << "Your height in meters: " << heightInMeters << std::endl;
-  std::cout << "Your weight in kilograms: " << weightInKilo << std::endl;
-  std::cout << "Your body mass index: " << bodyMassIndex << std::endl;
+  std::cout << "Enter a latitude in degrees, minutes, and seconds: " << std::endl;
+  std::cout << "First, enter the degrees: ";
+  int degrees = getNumber();
+  std::cout << "Next, enter the minutes of arc: ";
+  int minutes = getNumber();
+  std::cout << "Finally, enter the seconds of arc: ";
+  int seconds = getNumber();
+  float coef = 60.0;
+  float latitudeInDeegres = degrees + ((minutes + (seconds / coef)) / coef);
+  std::cout << degrees << " degrees, " << minutes << " minutes, " << seconds << " seconds = " << latitudeInDeegres << " degrees";
   return 0;
 }   
